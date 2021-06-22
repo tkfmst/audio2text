4ca27ec4d6f7bd942bf236cbd216c55f8a0d25e9
takafumi-s <takafumi.s.dev@gmail.com>
2021-06-22 10:04:39 +0900

    Add word search process

--- cmd/livecaption/main.go
+++ cmd/livecaption/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	speech "cloud.google.com/go/speech/apiv1"
 	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
@@ -23,10 +24,14 @@ func main() {
 	flag.Parse()
 
 	// ここを2にして検索文字列を受け取る
-	if len(flag.Args()) != 1 {
+	if len(flag.Args()) != 2 {
 		log.Fatal("Please pass path to your local audio file as a command line argument")
 	}
 	audioFile := flag.Arg(0)
+	log.Println("DEBUG", audioFile)
+
+	searchWord := flag.Arg(1)
+	log.Println("DEBUG", searchWord)
 
 	// ファイル読み込み
 	f, err := os.Open(audioFile)
@@ -34,6 +39,8 @@ func main() {
 		log.Fatal(err)
 	}
 	defer f.Close()
+
+	log.Println("DEBUG", 1)
 
 	// GCP setup config
 	ctx := context.Background()
@@ -53,9 +60,10 @@ func main() {
 		StreamingRequest: &speechpb.StreamingRecognizeRequest_StreamingConfig{
 			StreamingConfig: &speechpb.StreamingRecognitionConfig{
 				Config: &speechpb.RecognitionConfig{
-					Encoding:        speechpb.RecognitionConfig_LINEAR16,
-					SampleRateHertz: 16000,
-					LanguageCode:    "ja-JP", // https://cloud.google.com/speech-to-text/docs/languages
+					Encoding:              speechpb.RecognitionConfig_LINEAR16,
+					SampleRateHertz:       8000,
+					LanguageCode:          "ja-JP", // https://cloud.google.com/speech-to-text/docs/languages
+					EnableWordTimeOffsets: true,
 				},
 			},
 		},
@@ -68,7 +76,9 @@ func main() {
 		buf := make([]byte, 1024)
 		for {
 			n, err := f.Read(buf)
+			log.Println("DEBUG", n)
 			if n > 0 {
+				log.Println(buf[:n])
 				if err := stream.Send(&speechpb.StreamingRecognizeRequest{
 					StreamingRequest: &speechpb.StreamingRecognizeRequest_AudioContent{
 						AudioContent: buf[:n],
@@ -78,6 +88,7 @@ func main() {
 				}
 			}
 			if err == io.EOF {
+				log.Println("DEBUG", "EOF")
 				// Nothing else to pipe, close the stream.
 				if err := stream.CloseSend(); err != nil {
 					log.Fatalf("Could not close stream: %v", err)
@@ -91,10 +102,15 @@ func main() {
 		}
 	}()
 
+	// 最初から11にしておいてshiftしてもエラーにしない
+	result := make([]string, 11)
+
 	// response process
 	for {
 		resp, err := stream.Recv()
 		if err == io.EOF {
+			log.Println("DEBUG", "stream", "EOF")
+			// FIXME 残ったWordを処理する
 			break
 		}
 		if err != nil {
@@ -103,8 +119,23 @@ func main() {
 		if err := resp.Error; err != nil {
 			log.Fatalf("Could not recognize: %v", err)
 		}
-		for _, result := range resp.Results {
-			fmt.Printf("Result: %+v\n", result)
+		for _, res := range resp.Results {
+			for _, alt := range res.Alternatives {
+				for _, winfo := range alt.Words {
+					fmt.Println("DEBUG", winfo.Word)
+					fmt.Println("DEBUG", strings.Split(winfo.Word, "|")[0])
+
+					result = result[1:]
+					result = append(result, strings.Split(winfo.Word, "|")[0])
+
+					fmt.Println("DEBUG", result[5])
+					fmt.Println("DEBUG", len(result))
+					if len(result) == 11 && result[5] == searchWord {
+						log.Println("[Result]", "search=${searchWord}", strings.Trim(strings.Join(result, " "), " "))
+					}
+				}
+			}
+
 		}
 	}
 }
