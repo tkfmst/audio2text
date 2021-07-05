2618e4991040a2c4824657272df69c681753fb37
takafumi-s <takafumi.s.dev@gmail.com>
2021-07-05 13:28:43 +0900

    Remove Presenter interface

--- internal/adapter/presenter/presenter.go
+++ internal/adapter/presenter/presenter.go
@@ -1,23 +1,22 @@
 package presenter
 
-import "example.com/audio2text/internal/app/data/output"
-
-type Presenter interface {
-	Write(r output.Result)
-}
+import (
+	boundry_output "example.com/audio2text/internal/app/boundary/output"
+	"example.com/audio2text/internal/app/data/output"
+)
 
 type ResultPresenter struct {
 	dest IO
 }
 
-func NewResultPresenter(io IO) Presenter {
+func NewResultPresenter(io IO) boundry_output.Audio2Text {
 	pr := new(ResultPresenter)
 	pr.dest = io
 
 	return pr
 }
 
-func (pr *ResultPresenter) Write(r output.Result) {
+func (pr *ResultPresenter) OutputStdOut(r output.Result) {
 	if r.NonEmpty() {
 		pr.dest.Output(r.String())
 	}
--- internal/app/boundary/output/audio2text.go
+++ internal/app/boundary/output/audio2text.go
@@ -3,5 +3,5 @@ package output
 import "example.com/audio2text/internal/app/data/output"
 
 type Audio2Text interface {
-	Write(r output.Result)
+	OutputStdOut(r output.Result)
 }
