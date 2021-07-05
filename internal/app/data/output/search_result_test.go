20ff1524b26f24f902e423d48ee2298615e28123
takafumi-s <takafumi.s.dev@gmail.com>
2021-07-05 13:15:21 +0900

    Fix output test

--- internal/app/data/output/search_result_test.go
+++ internal/app/data/output/search_result_test.go
@@ -2,6 +2,7 @@ package output
 
 import (
 	"testing"
+	"time"
 
 	"example.com/audio2text/internal/entity"
 	"example.com/audio2text/internal/gomock/mock_entity"
@@ -27,15 +28,19 @@ func TestString2(t *testing.T) {
 	c := gomock.NewController(t)
 	defer c.Finish()
 
+	d1s, _ := time.ParseDuration("1s")
+	d2s, _ := time.ParseDuration("2s")
 	mockWord := mock_entity.NewMockWord(c)
 	mockWord.EXPECT().String().Return("a")
+	mockWord.EXPECT().StartTime().Return(d1s)
+	mockWord.EXPECT().EndTime().Return(d2s)
 
 	var ws = []entity.Word{mockWord}
 	s := entity.NewSearched(ws, 0, 1)
 	sr := NewSearchedResult(s)
 
 	obtained := sr.String()
-	expected := "[pos: 1, keyword: a] a"
+	expected := "[pos: 1, start: 1s, end: 2s, keyword: a] a"
 	if obtained != expected {
 		t.Errorf("Words.String() shoud return entity.Word.String() when Words.ws contains 1 entity.Word slice: obtained=`%+v`, expected=`%+v`", obtained, expected)
 	}
@@ -46,18 +51,23 @@ func TestString3(t *testing.T) {
 	c := gomock.NewController(t)
 	defer c.Finish()
 
-	mockWord1 := mock_entity.NewMockWord(c)
-	mockWord2 := mock_entity.NewMockWord(c)
+	d1s, _ := time.ParseDuration("1s")
+	d2s, _ := time.ParseDuration("2s")
 
+	mockWord1 := mock_entity.NewMockWord(c)
 	mockWord1.EXPECT().String().Return("a")
+
+	mockWord2 := mock_entity.NewMockWord(c)
 	mockWord2.EXPECT().String().Return("b")
+	mockWord2.EXPECT().StartTime().Return(d1s)
+	mockWord2.EXPECT().EndTime().Return(d2s)
 
 	var ws = []entity.Word{mockWord1, mockWord2}
 	s := entity.NewSearched(ws, 1, 2)
 	sr := NewSearchedResult(s)
 
 	obtained := sr.String()
-	expected := "[pos: 2, keyword: b] a b"
+	expected := "[pos: 2, start: 1s, end: 2s, keyword: b] a b"
 	if obtained != expected {
 		t.Errorf("Words.String() shoud return entity.Word.String() when Words.ws contains 1 entity.Word slice: obtained=`%+v`, expected=`%+v`", obtained, expected)
 	}
@@ -68,10 +78,15 @@ func TestString4(t *testing.T) {
 	c := gomock.NewController(t)
 	defer c.Finish()
 
-	mockWord1 := mock_entity.NewMockWord(c)
-	mockWord2 := mock_entity.NewMockWord(c)
+	d1s, _ := time.ParseDuration("1s")
+	d2s, _ := time.ParseDuration("2s")
 
+	mockWord1 := mock_entity.NewMockWord(c)
 	mockWord1.EXPECT().String().Return("a")
+	mockWord1.EXPECT().StartTime().Return(d1s)
+	mockWord1.EXPECT().EndTime().Return(d2s)
+
+	mockWord2 := mock_entity.NewMockWord(c)
 	mockWord2.EXPECT().String().Return("b")
 
 	var ws = []entity.Word{nil, mockWord1, mockWord2, nil}
@@ -79,7 +94,7 @@ func TestString4(t *testing.T) {
 	sr := NewSearchedResult(s)
 
 	obtained := sr.String()
-	expected := "[pos: 4, keyword: a] a b"
+	expected := "[pos: 4, start: 1s, end: 2s, keyword: a] a b"
 	if obtained != expected {
 		t.Errorf("Words.String() shoud ignore nil: obtained=`%+v`, expected=`%+v`", obtained, expected)
 	}
